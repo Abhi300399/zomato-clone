bec56107a22d4718da0daaf45006ac0c0fcf23a9
Abhi300399 <abhishekhadawale99@gmail.com>
2021-09-12 02:19:13 +0530

    add statics and methods and create signup api

--- server/database/allModels.js
+++ server/database/allModels.js
@@ -2,3 +2,8 @@ import { FoodModel } from "./food";
 import { ImageModel } from "./images";
 import { UserModel } from "./user";
 import { RestaurantModel } from "./restuarant";
+import {MenuModel} from "./menu";
+import {ReviewModel} from "./reviews";
+import {OrderModel} from "./order";
+
+export {FoodModel,ImageModel,UserModel,RestaurantModel,MenuModel,ReviewModel,OrderModel};
