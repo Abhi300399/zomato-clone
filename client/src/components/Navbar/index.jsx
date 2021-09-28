3db39d0e56f1615970735237c3e26025ece59ff1
Abhi300399 <abhishekhadawale99@gmail.com>
2021-09-29 00:03:55 +0530

    add navbar

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import {FaUserAlt} from "react-icons/fa";
+import {HiLocationMarker} from "react-icons/hi";
+import {IoMdArrowDropdown,IoMdArrowDropup} from "react-icons/io";
+import {RiSearch2Line} from "react-icons/ri";
+const MobileNav=()=>{
+        return (
+            <div className="flex items-center justify-between w-full md:hidden">
+                  <div className="w-28">
+                        <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
+                  </div>
+                <div className="flex items-center gap-3">
+                     <button className="bg-zomato-400 text-white py-2 px-3 rounded-full">Use App</button>
+                        <span className="border p-2 border-gray-300 text-zomato-400 rounded-full">
+                            <FaUserAlt/>
+                        </span>
+                </div>
+            </div>
+        )
+};
+
+const MediumNav=()=>{
+    return <>
+        <div className="hidden gap-4 md:flex w-full items-center justify-between lg:w-3/4">
+                 <div className="w-28">
+                        <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
+                 </div>
+                 <div className="w-full bg-white shadow-md p-3 border border-gray-200 rounded flex items-center gap-3">
+                     <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+                        <span className="text-zomato-400"><HiLocationMarker/></span>
+                        <input type="text" placeholder="Benguluru"  className="focus:outline-none"/>
+                        <IoMdArrowDropdown/>
+                     </div>
+                     <div className="flex items-center gap-2 w-full">
+                         <RiSearch2Line/>
+                         <input type="search" placeholder="Search for restuarant,cuisine or a dish" className="w-full focus:outline-none"/>
+                     </div>
+                 </div>
+                     <div className="flex gap-2">
+                     <button className="text-gray-500 hover:text-gray-800 text-xl">Login</button>
+                     <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
+                     </div>
+        </div>
+    </>;
+}
+
+const LargeNav=()=>{
+    return <>
+    <div className="hidden gap-4 md:flex w-full items-center justify-between ">
+             <div className="w-28">
+                    <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
+             </div>
+             <div className="w-full bg-white shadow-md p-3 border border-gray-200 rounded flex items-center gap-3">
+                 <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
+                    <span className="text-zomato-400"><HiLocationMarker/></span>
+                    <input type="text" placeholder="Benguluru"/>
+                    <IoMdArrowDropdown/>
+                 </div>
+                 <div className="flex items-center gap-2">
+                     <RiSearch2Line/>
+                     <input type="search" placeholder="Search for restuarant,cuisine or a dish"/>
+                 </div>
+             </div>
+                 <div className="flex gap-2">
+                 <button className="text-gray-500 hover:text-gray-800 text-xl">Login</button>
+                 <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
+                 </div>
+    </div>
+    </>;
+}
+
+const Navbar=()=>{
+    return (<>
+        <nav className="p-4 flex items-center bg-white shadow-md w-full">
+                 <MobileNav />
+                 <MediumNav />
+        </nav>
+    </>
+    );
+};
+
+export default Navbar;
