a54efcde210ae2dd92ce0ccd98bdd3037274f49d
Abhi300399 <abhishekhadawale99@gmail.com>
2021-10-02 00:00:16 +0530

    fix navbar width issue

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -5,7 +5,7 @@ import {IoMdArrowDropdown,IoMdArrowDropup} from "react-icons/io";
 import {RiSearch2Line} from "react-icons/ri";
 const MobileNav=()=>{
         return (
-            <div className="flex items-center justify-between w-full md:hidden">
+            <div className="flex items-center justify-between w-full lg:hidden">
                   <div className="w-28">
                         <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
                   </div>
@@ -19,13 +19,14 @@ const MobileNav=()=>{
         )
 };
 
-const MediumNav=()=>{
+const LargeNav=()=>{
     return <>
-        <div className="hidden gap-4 md:flex w-full items-center justify-between lg:w-3/4">
+        <div className="container px-20 mx-auto">
+        <div className="hidden gap-4 lg:flex w-full items-center justify-between">
                  <div className="w-28">
                         <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
                  </div>
-                 <div className="w-full bg-white shadow-md p-3 border border-gray-200 rounded flex items-center gap-3">
+                 <div className="w-3/4 bg-white shadow-md p-3 border border-gray-200 rounded flex items-center gap-3">
                      <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
                         <span className="text-zomato-400"><HiLocationMarker/></span>
                         <input type="text" placeholder="Benguluru"  className="focus:outline-none"/>
@@ -36,44 +37,22 @@ const MediumNav=()=>{
                          <input type="search" placeholder="Search for restuarant,cuisine or a dish" className="w-full focus:outline-none"/>
                      </div>
                  </div>
-                     <div className="flex gap-2">
+                     <div className="ml-28 flex gap-4">
                      <button className="text-gray-500 hover:text-gray-800 text-xl">Login</button>
                      <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
                      </div>
         </div>
+        </div>
     </>;
 }
 
-const LargeNav=()=>{
-    return <>
-    <div className="hidden gap-4 md:flex w-full items-center justify-between ">
-             <div className="w-28">
-                    <img src="https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png" alt="logo" className="w-full h-full" />
-             </div>
-             <div className="w-full bg-white shadow-md p-3 border border-gray-200 rounded flex items-center gap-3">
-                 <div className="flex items-center gap-2 border-r-2 border-gray-300 pr-2">
-                    <span className="text-zomato-400"><HiLocationMarker/></span>
-                    <input type="text" placeholder="Benguluru"/>
-                    <IoMdArrowDropdown/>
-                 </div>
-                 <div className="flex items-center gap-2">
-                     <RiSearch2Line/>
-                     <input type="search" placeholder="Search for restuarant,cuisine or a dish"/>
-                 </div>
-             </div>
-                 <div className="flex gap-2">
-                 <button className="text-gray-500 hover:text-gray-800 text-xl">Login</button>
-                 <button className="text-gray-500 hover:text-gray-800 text-xl">Signup</button>
-                 </div>
-    </div>
-    </>;
-}
+
 
 const Navbar=()=>{
     return (<>
         <nav className="p-4 flex items-center bg-white shadow-md w-full">
                  <MobileNav />
-                 <MediumNav />
+                 <LargeNav />
         </nav>
     </>
     );
