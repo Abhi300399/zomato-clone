97f676062f5d4fd26b9f4275a87153583e842001
Abhi300399 <abhishekhadawale99@gmail.com>
2021-10-02 21:58:04 +0530

    active foodtabs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import HomeLayoutHOC from "./HOC/Home.Hoc";
 
 //Component
 import Temp from "./components/temp";
+import Master from "./components/master";
+
 
 function App() {
   return (
     <>
         <HomeLayoutHOC path="/" exact component={Temp} />
+        <HomeLayoutHOC path="/:type" exact component={Master}/> 
     </>
     );
 }
